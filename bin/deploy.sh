a49584c91126589e3e656c741da78033bb3175fe
igormandello <igormandello@gmail.com>
2019-07-26 23:37:29 -0300

    Updates deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,6 @@
-git checkout master
-git merge -Xtheirs develop
-sass sass:css
-rm -rf sass
-rm -rf bin
-git add -f .
+git checkout master &&
+git merge --squash -Xtheirs develop &&
+sass sass:css &&
+rm -rf sass bin .gitignore &&
+git add -f . &&
 git commit -m "Updates GitHub pages"

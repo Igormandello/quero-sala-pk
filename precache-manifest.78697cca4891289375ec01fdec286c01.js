b0c97d457d5b23395f9083371238d031ba839752
igormandello <igormandello@gmail.com>
2019-07-28 04:00:56 -0300

    Updates

---
+++ precache-manifest.78697cca4891289375ec01fdec286c01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d31a54d049532d2d76336ac79146e75b",
+    "url": "/quero-sala-pl/static/media/cross.d31a54d0.svg"
+  },
+  {
+    "revision": "582888010a94e187f594f4997919ece1",
+    "url": "/quero-sala-pl/static/media/check.58288801.svg"
+  },
+  {
+    "revision": "de830f735fe33540f650",
+    "url": "/quero-sala-pl/static/js/runtime~main.de830f73.js"
+  },
+  {
+    "revision": "9f416add01fb182d3a26",
+    "url": "/quero-sala-pl/static/js/main.9f416add.chunk.js"
+  },
+  {
+    "revision": "c297502014d7cee094d0",
+    "url": "/quero-sala-pl/static/js/1.c2975020.chunk.js"
+  },
+  {
+    "revision": "9f416add01fb182d3a26",
+    "url": "/quero-sala-pl/static/css/main.bf145e2b.chunk.css"
+  },
+  {
+    "revision": "c746e1dcc8e2749f5bec186dcfbcfee2",
+    "url": "/quero-sala-pl/index.html"
+  }
+];
